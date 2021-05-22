c5059f848efe50a5eec0785327c7c5e3232e797e
Tainá Higa <62307844+tainahiga@users.noreply.github.com>
2021-05-22 18:30:28 -0300

    Add files via upload

---
+++ ProjetoJavaFX.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE javafx;
+USE javafx;
+
+CREATE TABLE department (
+  Id int(11) NOT NULL AUTO_INCREMENT,
+  Name varchar(60) DEFAULT NULL,
+  PRIMARY KEY (Id)
+);
+
+CREATE TABLE seller (
+  Id int(11) NOT NULL AUTO_INCREMENT,
+  Name varchar(60) NOT NULL,
+  Email varchar(100) NOT NULL,
+  BirthDate datetime NOT NULL,
+  BaseSalary double NOT NULL,
+  DepartmentId int(11) NOT NULL,
+  PRIMARY KEY (Id),
+  FOREIGN KEY (DepartmentId) REFERENCES department (id)
+);
+
+DESC department;
+DESC seller;
